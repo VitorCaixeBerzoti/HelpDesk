1984b26ceab480f409216e614b4f44e3f4bc4d1b
Vitor Caixe Berzoti <vitorcaixe29@gmail.com>
2026-08-31 12:39:53 -0300

    feat: adiciona validações ao login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,17 +5,39 @@ function Login () {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
     const [erro, setErro] = useStatus('')
+    const [sucesso, setSucesso] =useState('')
 
     function handleLogin(event) {
         event.preventDefault()
 
         if(email === '' || senha === '') {
             setErro("Preencha todos os campos")
+            setSucesso('')
+            return
+        }
+        if (!email.includes('@')) {
+            setErro("Digite um email válido")
+            setSucesso('')
+            return
+        }
+        if (senha.length < 6) {
+            setErro("Precisa ter 6 ou mais caracteries")
+            setSucesso('')
+            return
+        }
+
+        const usuarioTeste = {
+            email: 'admin@helpdesk.com',
+            senha: '123456'
+            }
+        if ( email !== usuarioTeste.email|| senha !== usuarioTeste.senha) {
+            setErro("Email ou senha incorretos")
+            setSucesso('')
             return
         }
         setErro('')
-        console.log(email) 
-        console.log(senha)
+        setSucesso('Login realizado com sucesso')
+
 }
 
 
@@ -32,6 +54,7 @@ function Login () {
             onChange={(event) => setSenha(event.target.value)}/>
             <button type="submit">Entrar</button>
             <p>{erro}</p>
+            <p>{sucesso}</p>
         </form>
         </>
     )
