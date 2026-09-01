4b3f551c41180d6fb4d5ac7f03d554999d175fe0
Vitor Caixe <Vitorcaixe29@gmail.com>
2026-09-01 17:16:27 -0300

    feat: adiciona hash de senha com bcrypt

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import { prisma } from './lib/prisma.js'
+import bcrypt from 'bcrypt'
 
 const app = express()
 
@@ -13,20 +15,29 @@ app.get('/', (req, res) => {
     res.send('backend do HelpDesk funcionando!')
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { email, senha } = req.body
 
-    const ususarioTeste = {
-        email: 'admin@helpdesk.com',
-        senha: '123456'
-    }
-
-    if(email !== ususarioTeste.email || senha !== ususarioTeste.senha) {
+    const usuario = await prisma.usuario.findUnique({
+        where: {
+            email
+        }
+    })
+    
+    if(!usuario) {
         return res.status(401).json({
             mensagem: 'Email ou senha incorretos'
         })
     }
-
+    
+    const senhaCorreta = await bcrypt.compare(senha, usuario.senha)
+    
+    if(!senhaCorreta){
+        return res.status(401).json({
+            mensagem: 'Email ou senha incorretos'
+        })
+        
+    }
     return res.status(200).json({
         mensagem: 'Login realizado com sucesso'
     })
